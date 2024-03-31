fab1d2ea549f8ce614208f43a8dc852a86b927be
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-03-31 16:14:55 +0200

    Update places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,6 +8,7 @@ from models.city import City
 from models.place import Place
 from models.user import User
 
+
 @app_views.route('/cities/<city_id>/places', methods=['GET'])
 def get_city_places(city_id):
     """Retrieve all Place objects of a City"""
